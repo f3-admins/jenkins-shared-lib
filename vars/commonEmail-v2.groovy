081c527f4f97c5835edd642234503c87ffa1d228
*gUUS2WLbYY <wangty@f3ens.com>
2025-04-30 10:51:21 +0900

    Add commonEmail-v2.groovy for email notification support
    Introduce a reusable Groovy function to manage build status email notifications. Validates required parameters such as recipient, job name, build number, and status, and dynamically sets the subject based on build success or failure. Integrates with the `emailext` plugin for structured email sending.

---
+++ vars/commonEmail-v2.groovy
@@ -0,0 +1,30 @@
+// vars/commonEmail.groovy
+
+def call(Map params) {
+    if (!params?.to) {
+        error "Email recipient ('to') is required."
+    }
+    if (params?.status == null) { // Check for null explicitly since status is boolean
+        error "Build status ('status') is required."
+    }
+    if (!params?.job_name) {
+        error "Job name ('job_name') is required."
+    }
+    if (!params?.build_number) {
+        error "Build number ('build_number') is required."
+    }
+
+    // Determine status message based on the boolean value
+    def statusMessage = params.status ? "✅ SUCCESS" : "❌ FAILURE"
+
+    def subject = "${statusMessage}: ${params.job_name} #${params.build_number} Build Report"
+    
+    emailext(
+        to: params.to,
+        subject: subject,
+        mimeType: 'text/html',
+        body: '${SCRIPT, template="build_status-v2.groovy"}'
+        //body: '${SCRIPT, template="jenkins-generic-matrix-email-html.template"}',
+        variables: [logContent: params.logContent] // Passing logContent as a variable
+    )
+}
