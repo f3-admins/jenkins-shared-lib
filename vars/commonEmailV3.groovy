aee521a436317215fb5a40e2ed6b0577a5dbd3f0
*gUUS2WLbYY <wangty@f3ens.com>
2025-04-30 12:03:41 +0900

    Add commonEmailV3 shared library for email notifications
    Introduces a new shared library function `commonEmailV3` for sending build status notifications via email. It validates required parameters, constructs a detailed subject line, and utilizes the `emailext` plugin with an HTML body template. This enhances consistency and maintainability for email reporting logic.

---
+++ vars/commonEmailV3.groovy
@@ -0,0 +1,33 @@
+// vars/commonEmail.groovy
+
+def call(Map params) {
+    if (!params?.to) {
+        error "Email recipient ('to') is required."
+    }
+    if (params?.status == null) { // Check for null explicitly since status is boolean
+        error "Build status ('status') is required."
+    }
+    if (!params?.job_name) {
+        error "Job name ('job_name') is required."
+    }
+    if (!params?.build_number) {
+        error "Build number ('build_number') is required."
+    }
+
+    // Print the logContent to ensure it's being passed correctly
+    echo "Log Content: ${params.logContent}"
+    
+    // Determine status message based on the boolean value
+    def statusMessage = params.status ? "✅ SUCCESS" : "❌ FAILURE"
+
+    def subject = "${statusMessage}: ${params.job_name} #${params.build_number} Build Report"
+    
+    emailext(
+        to: params.to,
+        subject: subject,
+        mimeType: 'text/html',
+        //body: '${SCRIPT, template="build_status-v2.groovy"}'
+        //body: '${SCRIPT, template="jenkins-generic-matrix-email-html.template"}',
+        body: '${SCRIPT, template="groovy-html.template"}'
+    )
+}
